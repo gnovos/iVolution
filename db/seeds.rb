33995c34771948427cd449d15027d25a924ab4b0
Mason <freemason@gmail.com>
2012-10-14 13:50:52 -0700

    format

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-AdminUser.create!(:email => 'admin@casuallama.org',
-                  :password => 'password',
-                  :password_confirmation => 'password')
+AdminUser.create!(:email => 'admin@casuallama.org', :password => 'password', :password_confirmation => 'password')
 
